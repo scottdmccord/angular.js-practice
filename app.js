96ced5dd4cf5c5e6c1b787c75a38f3411cdf1dbe
Scott McCord <scott.sdm27@gmail.com>
2017-01-10 14:14:35 -0500

    set up controller

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-var app = angular.module('gemStore', []);
+(function(){
+  var app = angular.module('gemStore', []);
+
+  app.controller('StoreController', function(){
+    this.product = gem;
+  });
+
+  var gem = {
+    name: 'Dodecahedron',
+    price: 2.95,
+    description: "The prettiest gem to ever gem."
+  }
+
+})();
+
