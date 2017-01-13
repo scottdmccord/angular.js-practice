daad2be89b95a8b37889e2578d229997a9a62499
Scott McCord <scott.sdm27@gmail.com>
2017-01-13 15:03:09 -0500

    added review controller

--- app.js
+++ app.js
@@ -16,7 +16,16 @@
       return this.tab === checkTab;
     };
 
-  })
+  });
+
+  app.controller('ReviewController', function(){
+    this.review = {};
+
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
 
   var gems = [
     { name: 'Dodecahedron',
