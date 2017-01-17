b5e036ba683f887468c67748d77005b6eacfa71b
Scott McCord <scott.sdm27@gmail.com>
2017-01-17 17:35:41 -0500

    added directive for product specs

--- app.js
+++ app.js
@@ -35,6 +35,13 @@
     };
   });
 
+  app.directive("productSpecs", function() {
+    return {
+      restrict: 'A',
+      templateUrl: "product-specs.html"
+    };
+  });
+
   var gems = [
     { name: 'Dodecahedron',
       price: 2.95,
